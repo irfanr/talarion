a50d238d68b6b0691c8ac06c9d1bbe83373aded0
irfanr <irfan.romadona@gmail.com>
2015-05-23 14:04:49 +0700

    added book

---
+++ src/main/webapp/scripts/components/entities/book/book.service.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('punicApp')
+    .factory('Book', function ($resource) {
+        return $resource('api/books/:id', {}, {
+            'query': { method: 'GET', isArray: true},
+            'get': {
+                method: 'GET',
+                transformResponse: function (data) {
+                    data = angular.fromJson(data);
+                    if (data.publicationDate != null){
+                        var publicationDateFrom = data.publicationDate.split("-");
+                        data.publicationDate = new Date(new Date(publicationDateFrom[0], publicationDateFrom[1] - 1, publicationDateFrom[2]));
+                    }
+                    return data;
+                }
+            },
+            'update': { method:'PUT' }
+        });
+    });
