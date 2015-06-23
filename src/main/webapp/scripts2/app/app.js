852b3e3b87770a0a25a7816ca4cba58487238d06
irfanr <irfan.romadona@gmail.com>
2015-06-23 11:22:50 +0700

    If we have an unauthorized request we redirect to the login page Don't do this check on the account API to avoid infinite loop

--- src/main/webapp/scripts2/app/app.js
+++ src/main/webapp/scripts2/app/app.js
@@ -45,6 +45,25 @@ angular.module('talarionApp', ['LocalStorageModule', 'tmh.dynamicLocale',
             }
         };
     })
+    .factory('authExpiredInterceptor', function ($rootScope, $q, $injector, localStorageService) {
+		return {
+			responseError: function(response) {
+				// If we have an unauthorized request we redirect to the login page
+				// Don't do this check on the account API to avoid infinite loop
+				if (response.status == 401 && response.data.path!="/api/account"){
+					var Auth = $injector.get('Auth');
+					var $state = $injector.get('$state');
+					var to = $rootScope.toState;
+					var params = $rootScope.toStateParams;
+					Auth.logout();
+					$rootScope.returnToState = to;
+					$rootScope.returnToStateParams = params;
+					$state.go('login');
+				}
+				return $q.reject(response);
+			}
+		};
+	})    
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider, $translateProvider, tmhDynamicLocaleProvider, httpRequestInterceptorCacheBusterProvider) {
 
         //enable CSRF
@@ -77,6 +96,7 @@ angular.module('talarionApp', ['LocalStorageModule', 'tmh.dynamicLocale',
             }
         });
 
+        $httpProvider.interceptors.push('authExpiredInterceptor');
 
         // Initialize angular-translate
         $translateProvider.useLoader('$translatePartialLoader', {
