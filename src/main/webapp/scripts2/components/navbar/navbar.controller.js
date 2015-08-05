5dd4b5238c2f0d3fc6688c0aa29fec4facf432d7
irfanr <irfan.romadona@gmail.com>
2015-08-05 14:37:47 +0700

    Refine user profile

--- src/main/webapp/scripts2/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts2/components/navbar/navbar.controller.js
@@ -4,6 +4,10 @@ angular.module('talarionApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
+        
+        Principal.identity().then(function(account) {
+            $scope.account = account;
+        });        
 
         $scope.logout = function () {
             Auth.logout();
